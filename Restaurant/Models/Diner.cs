1cf5ef553270d6ea0b816f2f02e07d878a426c69
p2nts <gabrieltucker260@gmail.com>
2024-01-31 11:45:33 -0800

    added model for diner
    Co-authored-by: Teddy Peterschmidt <t.a.peterschmidt@gmail.com>

---
+++ Restaurant/Models/Diner.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace Restaurant.Models
+{
+    public class Diner
+    {
+        public int DinerId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Specialty { get; set; }
+        public Cuisine Cuisine {get; set;}
+    }
+}
