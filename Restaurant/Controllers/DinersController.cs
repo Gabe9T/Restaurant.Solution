64df46bbec49b7a5bab19296d55ae038bd0beedb
p2nts <gabrieltucker260@gmail.com>
2024-01-31 11:44:35 -0800

    added DinersController
    Co-authored-by: Teddy Peterschmidt <t.a.peterschmidt@gmail.com>

---
+++ Restaurant/Controllers/DinersController.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Restaurant.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Restaurant.Controllers
+{
+    public class DinersController : Controller 
+    {
+        private readonly RestaurantContext _db;
+        public DinersController(RestaurantContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Diner> model = _db.Diners.Include(diner => diner.Cuisine).ToList();
+            return View(model);
+        }
+    }
+}
