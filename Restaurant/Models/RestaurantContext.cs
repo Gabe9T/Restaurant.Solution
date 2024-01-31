0450f4579e3f44a65a4adb47b5b2c578d50a42ec
p2nts <gabrieltucker260@gmail.com>
2024-01-31 14:17:19 -0800

    added diner list
    Co-authored-by: Teddy Peterschmidt <t.a.peterschmidt@gmail.com>

--- Restaurant/Models/RestaurantContext.cs
+++ Restaurant/Models/RestaurantContext.cs
@@ -5,7 +5,7 @@ namespace Restaurant.Models
   public class RestaurantContext : DbContext
   {
     public DbSet<Cuisine> Cuisines { get; set; }
-    // public DbSet<Item> Items { get; set; }
+    public DbSet<Diner> Diners { get; set; }
 
     public RestaurantContext(DbContextOptions options) : base(options) { }
   }
