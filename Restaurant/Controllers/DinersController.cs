0fd59b6eba70406d22d59194bd06094003a82200
p2nts <gabrieltucker260@gmail.com>
2024-02-01 14:46:18 -0800

    added complete dinercontroller
    Co-authored-by: Teddy Peterschmidt <t.a.peterschmidt@gmail.com>

--- Restaurant/Controllers/DinersController.cs
+++ Restaurant/Controllers/DinersController.cs
@@ -18,23 +18,59 @@ namespace Restaurant.Controllers
         public ActionResult Index()
         {
             List<Diner> model = _db.Diners.Include(diner => diner.Cuisine).ToList();
+            ViewBag.PageTitle = "View All Items";
             return View(model);
+    
         }
 
-        public ActionResult Create(int id)
+        public ActionResult Create()
         {
-            ViewBag.Cuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+            ViewBag.CuisineId = new SelectList(_db.Cuisines, "CuisineId", "Name");
             return View();
         }
-        // Cuisine cuisine = new SelectList(_db.Cuisines, "CuisineId", "Name");
+ 
         [HttpPost]
         public ActionResult Create(Diner diner)
         {
-            if (diner.DinerId == 0)
+            if (diner.CuisineId == 0)
             {
                 return RedirectToAction("Create");
             }
             _db.Diners.Add(diner);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            Diner thisDiner = _db.Diners.FirstOrDefault(diner => diner.DinerId == id);
+            return View(thisDiner);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var thisDiner = _db.Diners.FirstOrDefault(diner => diner.DinerId == id);
+            return View(thisDiner);
+        }
+        [HttpPost]
+        public ActionResult Edit(Diner diner)
+        {
+            _db.Diners.Update(diner);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+        public ActionResult Delete(int id)
+        {
+            var thisDiner = _db.Diners.FirstOrDefault(diner=> diner.DinerId == id);
+            return View(thisDiner);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var thisDiner = _db.Diners.FirstOrDefault(diner => diner.DinerId == id);
+            _db.Diners.Remove(thisDiner);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
